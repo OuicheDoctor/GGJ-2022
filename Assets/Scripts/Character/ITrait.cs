31a458a90c7b3592df0d5d52a9da7a9d64809fdd
Corentin Dupont <corentin@helyx.io>
2022-01-22 17:53:39 +0100

    🚧 feat(characters): define basics properties

---
+++ Assets/Scripts/Character/ITrait.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ITrait
+{
+    public string Name { get; set; }
+    public string Category { get; set; }
+}
