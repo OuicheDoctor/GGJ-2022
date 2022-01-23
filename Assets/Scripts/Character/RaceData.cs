cdddad8d14bd889dcf7e5a9968342fb90f658a93
Corentin Dupont <corentin@helyx.io>
2022-01-23 19:52:02 +0100

    ✨ feat(Race): create RaceData ScriptableObject

---
+++ Assets/Scripts/Character/RaceData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+
+namespace GGJ.Characters {
+
+    [CreateAssetMenu(menuName = "GGJ/Race", fileName = "NewRace")]
+    public class RaceData : ScriptableObject, IRace
+    {
+        #region Exposed API
+
+        public string Name { get => _name; set => _name = value; }
+
+        #endregion
+
+        #region Inspector Fields
+
+        [SerializeReference] private string _name;
+
+        #endregion
+    }
+}
