260a4234243b87e326754157add3053ac401ba15
Lucas Junker <lucas.junker@seenapsis.studio>
2022-01-23 18:45:01 +0100

    Some helpers on collection and list

---
+++ Assets/Scripts/Helper/ListExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+public static class ListExtensions
+{
+    public static T PickOneAndRemove<T>(this List<T> source)
+    {
+        T picked = source.PickOne();
+        source.Remove(picked);
+        return picked;
+    }
+
+    public static void Shuffle<T>(this List<T> source)
+    {
+        var count = source.Count;
+        var last = count - 1;
+        for (var i = 0; i < last; ++i)
+        {
+            var r = UnityEngine.Random.Range(i, count);
+            var tmp = source[i];
+            source[i] = source[r];
+            source[r] = tmp;
+        }
+    }
+}
