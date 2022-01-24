761e2db20e374fbcb5ba6bce5ac9b822b121328f
Corentin Dupont <corentin@helyx.io>
2022-01-24 22:51:19 +0100

    ✨ feat(races): create race names data scriptable object

---
+++ Assets/Scripts/Race/RaceNamesData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GGJ.Races
+{
+    public class RaceNamesData: ScriptableObject
+    {
+        #region Exposed API
+
+        public List<string> Names { get => _names; set => _names = value; }
+
+        #endregion
+
+
+        #region Inspector Fields
+
+        [SerializeReference] List<string> _names;
+
+        #endregion
+    }
+}
