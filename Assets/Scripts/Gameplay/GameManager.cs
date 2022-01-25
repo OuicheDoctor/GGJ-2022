cae6db3573141fa6d79b977618d7aa74ef1e9ab6
ysaunier <yoann@ysaunier.dev>
2022-01-25 18:24:20 -0500

    Refactor and Run the Algorithm on Start Game

--- Assets/Scripts/Gameplay/GameManager.cs
+++ Assets/Scripts/Gameplay/GameManager.cs
@@ -1,3 +1,7 @@
+using GGJ.Characters;
+using GGJ.Matchmaking;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -25,6 +29,7 @@ public class GameManager : MonoBehaviour
         _uiManager.SetMainMenuVisible(false);
         _secondsBuffer = 0;
         enabled = true;
+        GenerateMonstersAndSolution();
     }
 
     public void OnButtonNextDayClick()
@@ -34,6 +39,16 @@ public class GameManager : MonoBehaviour
         CurrentHour = _startingHour;
         _secondsBuffer = 0;
         enabled = true;
+    }
+    public void GenerateMonstersAndSolution()
+    {
+        var generation = CharactersGenerationManager.Instance.GenerateCharactersWithForm(8);
+        IList<ICharacter> characters = generation.ConvertAll<ICharacter>(e => e.character);
+        PartenerCollection parteners = BruteForcePairMatching.Instance.Process(characters);
+        foreach (var partener in parteners)
+        {
+            Debug.Log(partener);
+        }
     }
 
     public void OnButtonBackToMainMenuClick()
--- Assets/Scripts/Matchmaking/PartenerCollection.cs
+++ Assets/Scripts/Matchmaking/PartenerCollection.cs
@@ -1,22 +1,43 @@
 using GGJ.Characters;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace GGJ.Matchmaking
 {
-    public class PartenerCollection : Dictionary<Tuple<ICharacter, ICharacter>, int>
+
+    public struct Partener
     {
+        public Partener(ICharacter character1, ICharacter character2, int score)
+        {
+            Character1 = character1;
+            Character2 = character2;
+            Score = score;
+        }
+
+        public ICharacter Character1 { get; private set; }
+        public ICharacter Character2 { get; private set; }
+
+        public int Score { get; private set; }
+
+        public override string ToString() => $"{Character1.Name} x {Character2.Name} : {Score}";
+    }
+
+    public class PartenerCollection : IEnumerable<Partener>
+    {
+        private Dictionary<Tuple<ICharacter, ICharacter>, int> _parteners;
         private Dictionary<ICharacter, int> _counters = new Dictionary<ICharacter, int>();
         private int _limit = 0;
 
         public PartenerCollection(int limit)
         {
+            _parteners = new Dictionary<Tuple<ICharacter, ICharacter>, int>();
             _limit = limit;
         }
         public bool IsPartener(ICharacter character)
         {
-            return Keys.ToList().Where(e => e.Item1 == character || e.Item2 == character).Any();
+            return _parteners.Keys.ToList().Where(e => e.Item1 == character || e.Item2 == character).Any();
         }
 
         public bool RemoveByCharacter(ICharacter character)
@@ -26,11 +47,11 @@ namespace GGJ.Matchmaking
                 return false;
             }
 
-            List<Tuple<ICharacter, ICharacter>> keysToRemove = Keys.ToList().Where(e => e.Item1 == character || e.Item2 == character).ToList();
+            List<Tuple<ICharacter, ICharacter>> keysToRemove = _parteners.Keys.ToList().Where(e => e.Item1 == character || e.Item2 == character).ToList();
 
             foreach (Tuple<ICharacter, ICharacter> keyToRemove in keysToRemove)
             {
-                base.Remove(keyToRemove);
+                _parteners.Remove(keyToRemove);
             }
             _counters[character] += 1;
             return true;
@@ -38,8 +59,8 @@ namespace GGJ.Matchmaking
 
         public int GetActualScore(ICharacter character)
         {
-            Tuple<ICharacter, ICharacter> key = Keys.Where(e => e.Item1 == character || e.Item2 == character).First();
-            return this[key];
+            Tuple<ICharacter, ICharacter> key = _parteners.Keys.Where(e => e.Item1 == character || e.Item2 == character).First();
+            return _parteners[key];
         }
 
         public bool IsLocked(ICharacter character)
@@ -51,10 +72,30 @@ namespace GGJ.Matchmaking
             return _counters[character] >= _limit;
         }
 
-        public new bool ContainsKey(Tuple<ICharacter, ICharacter> key)
+        public bool ContainsKey(ICharacter character1, ICharacter character2)
         {
-            Tuple<ICharacter, ICharacter> variant = new Tuple<ICharacter, ICharacter>(key.Item2, key.Item1);
-            return base.ContainsKey(key) || base.ContainsKey(variant);
+            Tuple<ICharacter, ICharacter> key = new Tuple<ICharacter, ICharacter>(character1, character2);
+            Tuple<ICharacter, ICharacter> variant = new Tuple<ICharacter, ICharacter>(character2, character1);
+            return _parteners.ContainsKey(key) || _parteners.ContainsKey(variant);
+        }
+
+        public IEnumerator<Partener> GetEnumerator()
+        {
+            foreach (Partener partener in _parteners.ToList().Select(i => new Partener(i.Key.Item1, i.Key.Item2, i.Value)))
+            {
+                yield return partener;
+            }
+        }
+
+        public void Add(ICharacter character1, ICharacter character2, int score)
+        {
+            Tuple<ICharacter, ICharacter> key = new Tuple<ICharacter, ICharacter>(character1, character2);
+            _parteners.Add(key, score);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
 }
