0e87267792aa00467e2b3d73023fd239e6ba8c67
Corentin Dupont <corentin@helyx.io>
2022-01-26 08:53:43 +0100

    ✨feat(form UI): display race name

--- Assets/Scripts/UI/UIFormDoc.cs
+++ Assets/Scripts/UI/UIFormDoc.cs
@@ -10,10 +10,12 @@ public class UIFormDoc : MonoBehaviour
     [Header("Forms Elements")]
     [SerializeField] private Image _characterImage;
     [SerializeField] private TextMeshProUGUI _characterNameText;
+    [SerializeField] private TextMeshProUGUI _characterRaceText;
 
     public void FillForm(Character character, GeneratedForm form)
     {
         _characterImage.sprite = character.Race.Drawing;
         _characterNameText.text = character.Name;
+        _characterRaceText.text = character.Race.Name;
     }
 }
