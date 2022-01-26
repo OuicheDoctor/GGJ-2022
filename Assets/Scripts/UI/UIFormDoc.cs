b3c44466027f2b0a6e7825539fd7824d0ef1c401
Corentin Dupont <corentin@helyx.io>
2022-01-26 09:11:12 +0100

    ✨ feat(forms UI): Hobbies

--- Assets/Scripts/UI/UIFormDoc.cs
+++ Assets/Scripts/UI/UIFormDoc.cs
@@ -1,9 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using GGJ.Characters;
 using TMPro;
+using System.Linq;
+using System.Collections.Generic;
+using GGJ.Hobbies;
 
 public class UIFormDoc : MonoBehaviour
 {
@@ -11,11 +12,17 @@ public class UIFormDoc : MonoBehaviour
     [SerializeField] private Image _characterImage;
     [SerializeField] private TextMeshProUGUI _characterNameText;
     [SerializeField] private TextMeshProUGUI _characterRaceText;
+    [SerializeField] private TextMeshProUGUI _characterHobbiesText;
 
     public void FillForm(Character character, GeneratedForm form)
     {
         _characterImage.sprite = character.Race.Drawing;
         _characterNameText.text = character.Name;
         _characterRaceText.text = character.Race.Name;
+
+        var hobbiesName = (character.Hobbies as List<HobbyData>).ConvertAll(h => h.hobbyName);
+        _characterHobbiesText.text = string.Join(", ", hobbiesName);
+        
+        
     }
 }
