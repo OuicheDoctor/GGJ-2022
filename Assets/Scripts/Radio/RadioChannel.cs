259570ea7a4accfca9fdef0b17850a5cddd95484
Corentin Dupont <corentin@helyx.io>
2022-01-29 15:25:36 +0100

    RadioChannel assets

---
+++ Assets/Scripts/Radio/RadioChannel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "GGJ/Radio/Radio Channel", fileName = "NewRadioChannel")]
+public class RadioChannel: ScriptableObject
+{
+    public List<AudioClip> musics;
+}
