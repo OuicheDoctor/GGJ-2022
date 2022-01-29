932cce813583f31cd5adf8e4c26386055f523cde
Corentin Dupont <corentin@helyx.io>
2022-01-29 17:35:55 +0100

    init radio channel sources

---
+++ Assets/Scripts/Radio/RadioChannelSource.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct RadioChannelSource
+{
+    public RadioChannel RadioChannel { get; }
+    public AudioSource AudioSource { get; }
+    public List<AudioClip> RemainingClips { get; }
+
+
+    public RadioChannelSource(RadioChannel radioChannel, AudioSource audioSource, List<AudioClip> remainingClips)
+    {
+        this.RadioChannel = radioChannel;
+        this.AudioSource = audioSource;
+        this.RemainingClips = remainingClips;
+    }
+}
--- Assets/Scripts/Radio/RadioManager.cs
+++ Assets/Scripts/Radio/RadioManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Linq;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class RadioManager: MonoBehaviour
@@ -6,13 +7,32 @@ public class RadioManager: MonoBehaviour
     public static RadioManager Instance { get; private set; }
 
     private RadioChannel _activeChannel;
+    private List<RadioChannelSource> _radioChannelSources;
 
     [SerializeField] private AudioSettings _audioSettings;
+    [SerializeField] private List<AudioSource> _audioSources;
+
+    private void Start()
+    {
+        InitRadioChannelSource();
+    }
 
     public void ChangeChannel(int channelIndex)
     {
         _activeChannel = _audioSettings.RadioChannels[channelIndex];
         Debug.Log(_activeChannel);
+    }
+
+    private void InitRadioChannelSource()
+    {
+        foreach (var item in _audioSettings.RadioChannels.Select((value, i) => new { value, i }))
+        {
+            var radioChannelSource = new RadioChannelSource(
+                item.value,
+                _audioSources[item.i],
+                item.value.Musics
+            );
+        }
     }
 
     private void Awake()
