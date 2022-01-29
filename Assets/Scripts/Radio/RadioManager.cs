3d5aeb1b3ad9d0f8e4df7470397412c5896a18ce
Corentin Dupont <corentin@helyx.io>
2022-01-29 17:02:56 +0100

    set channel from radio interface

---
+++ Assets/Scripts/Radio/RadioManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public class RadioManager: MonoBehaviour
+{
+    public static RadioManager Instance { get; private set; }
+
+    private RadioChannel _activeChannel;
+
+    [SerializeField] private AudioSettings _audioSettings;
+
+    public void ChangeChannel(int channelIndex)
+    {
+        _activeChannel = _audioSettings.RadioChannels[channelIndex];
+        Debug.Log(_activeChannel);
+    }
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+}
---
+++ Assets/Scripts/Radio/UIRadio.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+public class UIRadio: MonoBehaviour
+{
+    public void OnChannelButtonClick(int channelIndex)
+    {
+        RadioManager.Instance.ChangeChannel(channelIndex);
+    }
+}
