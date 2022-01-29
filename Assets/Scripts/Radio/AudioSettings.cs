b99c7d47abcc2d6cad6d3975f0cc6646c0e6693a
Corentin Dupont <corentin@helyx.io>
2022-01-29 15:52:52 +0100

    audio settings

---
+++ Assets/Scripts/Radio/AudioSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "GGJ/Audio Settings", fileName = "NewAudioSettings")]
+public class AudioSettings: ScriptableObject
+{
+    public List<RadioChannel> RadioChannels => _radioChannels;
+
+    [SerializeReference] private List<RadioChannel> _radioChannels; 
+}
