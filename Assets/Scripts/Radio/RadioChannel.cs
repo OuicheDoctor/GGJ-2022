b22144fb5a5d2e13c1ca286606d55e5e26875840
Corentin Dupont <corentin@helyx.io>
2022-01-29 15:45:46 +0100

    Radio jingles struct

--- Assets/Scripts/Radio/RadioChannel.cs
+++ Assets/Scripts/Radio/RadioChannel.cs
@@ -5,5 +5,9 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "GGJ/Radio/Radio Channel", fileName = "NewRadioChannel")]
 public class RadioChannel: ScriptableObject
 {
-    public List<AudioClip> musics;
+    public List<AudioClip> Musics => _musics;
+    public List<RadioJingle> jingles => _jingles;
+
+    [SerializeReference] private List<AudioClip> _musics;
+    [SerializeField] private List<RadioJingle> _jingles;
 }
---
+++ Assets/Scripts/Radio/RadioJingle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public struct RadioJingle
+{
+    public int Hour => _hour;
+    public AudioClip Audio => _audio;
+
+    [SerializeField] private int _hour;
+    [SerializeField] private AudioClip _audio;
+}
