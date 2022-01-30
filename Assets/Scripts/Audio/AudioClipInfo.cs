22a539dd786f24e9c493440e0f3951e85a12338a
Corentin Dupont <corentin@helyx.io>
2022-01-30 10:52:49 +0100

    update audio settings with bgm and sfx

---
+++ Assets/Scripts/Audio/AudioClipInfo.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioClipInfo : ScriptableObject
+{
+    public string Name => _name;
+    public AudioClip AudioFile => _audioFile;
+
+    [SerializeReference] string _name;
+    [SerializeReference] AudioClip _audioFile;
+}
--- Assets/Scripts/Audio/AudioSettings.cs
+++ Assets/Scripts/Audio/AudioSettings.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 public class AudioSettings: ScriptableObject
 {
     public List<RadioChannel> RadioChannels => _radioChannels;
+    public List<AudioClipInfo> BackgroundMusics => _backgroundMusics;
+    public List<AudioClipInfo> SoundEffects => _soundEffects;
 
     [SerializeReference] private List<RadioChannel> _radioChannels; 
+    [SerializeReference] private List<AudioClipInfo> _backgroundMusics;
+    [SerializeReference] private List<AudioClipInfo> _soundEffects;
 }
